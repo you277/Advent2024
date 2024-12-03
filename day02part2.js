05f232c68d0e0088ea85e25bd5647a1061dae4b5
you <144391848+you277@users.noreply.github.com>
2024-12-02 19:31:51 -0500

    Update day02part2.js

--- day02part2.js
+++ day02part2.js
@@ -1,52 +1,78 @@
-// incorrect rn
+// still not correct
 
 const fs = require("fs")
 
 const data = fs.readFileSync("input", "utf-8")
 
-function isIncreasing(nums) {
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] > nums[i - 1]) {
-            return false
-        }
-    }
-    return true
-}
-
-function isDecreasing(nums) {
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] < nums[i - 1]) {
-            return false
-        }
-    }
-    return true
-}
-
-function isSafe(nums) {
-    let removedSafe = false
-    for (let i = 1; i < nums.length; i++) {
-        let diff = Math.abs(nums[i] - nums[i - 1])
-        if (diff < 1 || diff > 3) {
-            if (removedSafe) {
-                console.log(nums, "still no")
-                return false
-            }
-            console.log(nums, "removed", nums[i])
-            removedSafe = true
-            nums.splice(i, 1)
-            console.log(nums)
+function iter(data, callback) {
+    for (let i = 1; i < data.nums.length; i++) {
+        if (!callback(data.nums[i - 1], data.nums[i])) {
+            // return false
+            if (data.ignored) return false
+            data.ignored = true
+            data.nums.splice(i, 1)
             i--
         }
     }
-    console.log(nums, "ok")
+    return true
+}
+
+function isIncreasing(data) {
+    return iter(data, (a, b) => a < b)
+    for (let i = 1; i < data.nums.length; i++) {
+        if (data.nums[i] > data.nums[i - 1]) {
+            if (data.ignored) {
+                return false
+            }
+            data.ignored = true
+            data.nums.splice(i, 1)
+            i--
+        }
+    }
+    return true
+}
+
+function isDecreasing(data) {
+    return iter(data, (a, b) => a > b)
+    for (let i = 1; i < data.nums.length; i++) {
+        if (data.nums[i] < data.nums[i - 1]) {
+            if (data.ignored) {
+                return false
+            }
+            data.ignored = true
+            data.nums.splice(i, 1)
+            i--
+        }
+    }
+    return true
+}
+
+function isSafe(data) {
+    return iter(data, (a, b) => {
+        let diff = Math.abs(a - b)
+        return diff >= 1 && diff <= 3
+    })
+    for (let i = 1; i < data.nums.length; i++) {
+        let diff = Math.abs(data.nums[i] - data.nums[i - 1])
+        if (diff < 1 || diff > 3) {
+            if (data.ignored) {
+                return false
+            }
+            data.nums.ignored = true
+            data.nums.splice(i, 1)
+            i--
+        }
+    }
     return true
 }
 
 let safe = 0
 
-for (let line of data.split("\r\n")) {
-    let nums = line.split(" ").map(n => parseInt(n))
-    if (isSafe(nums) && (isIncreasing(nums) || isDecreasing(nums))) {
+for (let line of data.split("\n")) {
+    let lines = line.split(" ")
+    if (lines[0] === "") continue
+    let data = {nums: lines.map(n => parseInt(n)), ignored: false}
+    if (isSafe(data) && ((isIncreasing(data) || isDecreasing(data)))) {
         safe++
     }
 }
