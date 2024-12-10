30ba7dc080b2746e96049798bdd020a3822525cc
you <144391848+you277@users.noreply.github.com>
2024-12-09 22:43:37 -0500

    Update 07-1.js

--- 07-1.js
+++ 07-1.js
@@ -2,62 +2,31 @@ const fs = require("fs")
 
 const fileData = fs.readFileSync("input", "utf-8")
 
-let result = 0
-
 let operators = [
     (a, b) => a * b,
     (a, b) => a + b
 ]
 
-// function loopThruOps(value, nums, prevNum, amtOps) {
-//     if (amtOps === 0) return
-//     for (let i = 0; i < 2; i++) {
-//         let newNum = operators[i](prevNum,)
-//         if (loopThruOps(value, nums, prevNum, amtOps - 1)) {
+let result = 0
 
-//         }
-//     }
-// }
-
-for (let line of fileData.split("\r\n")) {
+for (let line of fileData.split("\n")) {
     let fields = line.split(": ")
     let value = parseInt(fields[0])
     let nums = fields[1].split(" ").map(n => parseInt(n))
-    // let bin = nums.length.toString(2)
     let amtOfOperators = nums.length - 1
-    console.log(amtOfOperators)
-    for (let i in Array(1).fill(nums.length - 1)) {
+
+    for (let i = 0; i < 2**amtOfOperators; i++) {
         let bin = i.toString(2)
-        bin = bin.padStart(amtOfOperators - bin.length, "0")
-        console.log(amtOfOperators, amtOfOperators^2, i, bin)
+        let r = nums[0]
+        for (let v = 0; v < amtOfOperators; v++) {
+            let char = bin.charAt(bin.length - amtOfOperators + v) || "0"
+            r = operators[parseInt(char)](r, nums[v + 1])
+        }
+        if (r == value) {
+            result += value
+            break
+        }
     }
-    // for (let i = 0; i < amtOfOperators^2; i++) {
-    //     let bin = i.toString(2)
-    //     bin = bin.padStart(amtOfOperators - bin.length, "0")
-    //     console.log(amtOfOperators, amtOfOperators^2, i, bin)
-    // }
-    // for (let i = 0; i < 2^amtOfOperators; i++) {
-    //     let bin = i.toString(2)
-    //     console.log(bin)
-    //     let opResult = nums[0]
-    //     let wow = []
-    //     for (let v = 0; v < bin.length; v++) {
-    //         let char = bin.charAt(bin.length - amtOfOperators + v) || "0"
-    //         wow.push(char === "0" ? "*" : "+")
-    //         opResult = operators[parseInt(char)](opResult, nums[v + 1])
-    //     }
-    //     let r = nums[0].toString()
-    //     for (let i2 = 1; i2 < nums.length; i2++) {
-    //         r += ` ${wow[i2 - 1]} ${nums[i2]}`
-    //     }
-    //     // console.log(r)
-    //     if (opResult === value) {
-    //         console.log(value)
-    //         continue
-    //     }
-    // }
-    // for (let i = 0; i < nums.length - 1; i++) {
-    //     let char = i.toString(2).charAt(bin.length - 1 - i) || "0"
-    //     opResult = operators[parseInt(char)](opResult, nums[i + 1])
-    // }
 }
+
+console.log(result)
