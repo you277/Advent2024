05b4a23343984e1cec441e036ca766f84b25ac42
you <144391848+you277@users.noreply.github.com>
2024-12-13 00:26:19 -0500

    Update 11-2.js

--- 11-2.js
+++ 11-2.js
@@ -5,19 +5,26 @@ const fs = require("fs")
 let fileData = fs.readFileSync("input", "utf-8")
 let stones = fileData.split(" ").map(n => parseInt(n))
 
-let one = [1]
 let cache = new Map()
+let powCache = new Map()
 let floor = Math.floor
+let log = Math.log
+let LOG10E = Math.LOG10E
+
+function calcPow(l) {
+    let pow = 10**floor(l/2)
+    powCache.set(l, pow)
+    return pow
+}
+
+let one = [1]
 
 function calc(n) {
     if (!n) return one
-    let strN = n.toString()
-    if (strN.length % 2 === 0) {
-        let pow = 10**floor(strN.length/2)
-        return [
-            floor(n / pow),
-            n % pow
-        ]
+    let strL = log(n) * LOG10E + 1 | 0
+    if (!(strL % 2)) {
+        let pow = powCache.get(strL) ?? calcPow(strL)
+        return [ floor(n / pow), n % pow]
     }
     return [n * 2024]
 }
@@ -27,12 +34,25 @@ function go(n) {
     cache.set(n, v)
     return v
 }
+
+function gogogo(nums, amt, depth, maxDepth) {
+    if (depth == maxDepth) return amt + nums.length
+    let d2 = depth + 1
+    let n1 = nums[0]
+    amt = gogogo(cache.get(n1) ?? go(n1), amt, d2, maxDepth)
+    if (nums.length != 1) {
+        let n2 = nums[1]
+        amt = gogogo(cache.get(n2) ?? go(n2), amt, d2, maxDepth)
+    }
+    return amt
+}
+
 let aaa = performance.now()
 
-for (let i = 0; i < 75; i++) {
-    // console.log(i, stones.length)
-    stones = stones.flatMap(n => cache.get(n) ?? go(n))
+let totalStones = 0
+for (let n of stones) {
+    totalStones += gogogo([n], 0, 0, 75)
 }
 
 console.log(`time elapsed: ${performance.now() - aaa}`)
-console.log(stones.length)
+console.log(totalStones)
